0b380179d2c19721bce5288ab0e0db4e7bacb07a
Han Qin <hqin1021@live.com>
2016-08-24 12:05:01 -0400

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,20 @@
+﻿-- Table: reports
+
+DROP TABLE reports;
+
+CREATE TABLE reports
+(
+  id bigserial NOT NULL,
+  lat double precision,
+  lng double precision,
+  image0 text,
+  image1 text,
+  desciption text,
+  rep_time timestamp without time zone,
+  CONSTRAINT reports_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=TRUE
+);
+ALTER TABLE reports
+  OWNER TO postgres;
